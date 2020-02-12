1495583b4bdfd6951bda27d9f9cf7e7b6f1a455c
Vladimir Pecanac <vladimir.pecanac@gmail.com>
2020-02-12 13:04:26 +0100

    Contest challenge

---
+++ FebruaryContestEntry/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FebruaryContestEntry
+{
+	class Program
+	{
+		const char block = '▓';
+
+		static void Main(string[] args)
+		{
+			// The solution implementation goes here
+
+			Console.ResetColor();
+			Console.WriteLine();
+			Console.WriteLine();
+			Console.WriteLine("Yay, a colorful shape!");
+			Console.ReadLine();
+		}
+	}
+}
