d0a7cdafb8ce558687ddb8b1dcda65a10931bc63
Md Shiefuzzaman <shiefuzzaman.bd@gmail.com>
2020-02-13 22:04:26 -0600

    Draw 30x15 shape

--- FebruaryContestEntry/Program.cs
+++ FebruaryContestEntry/Program.cs
@@ -2,19 +2,39 @@
 
 namespace FebruaryContestEntry
 {
-	class Program
-	{
-		const char block = '▓';
+    class Program
+    {
+        const char block = '▓';
+        const int columns = 30;
+        const int rows = 15;
 
-		static void Main(string[] args)
-		{
-			// The solution implementation goes here
+        static void Main(string[] args)
+        {
+            // The solution implementation goes here
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.ForegroundColor = GetConsoleForegroundColor(i, j);
+                    Console.Write(block);
+                }
+                Console.WriteLine();
+            }
 
-			Console.ResetColor();
-			Console.WriteLine();
-			Console.WriteLine();
-			Console.WriteLine("Yay, a colorful shape!");
-			Console.ReadLine();
-		}
-	}
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Yay, a colorful shape!");
+            Console.ReadLine();
+        }
+        private static ConsoleColor GetConsoleForegroundColor(
+            int row,
+            int column)
+          => (row == 0 ||
+              row == rows - 1 ||
+              column == 0 ||
+              column == columns - 1)
+              ? ConsoleColor.Green
+              : ConsoleColor.Yellow;
+    }
 }
